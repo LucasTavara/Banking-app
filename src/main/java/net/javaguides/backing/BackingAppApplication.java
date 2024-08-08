87e236ed9847f16d20f77cf22c92013d737cf972
LucasTavares <lulucas25@hotmail.com>
2024-08-07 23:34:13 -0300

    created Project

---
+++ src/main/java/net/javaguides/backing/BackingAppApplication.java
@@ -0,0 +1,13 @@
+package net.javaguides.backing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/javaguides/backing/BackingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguides.backing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
